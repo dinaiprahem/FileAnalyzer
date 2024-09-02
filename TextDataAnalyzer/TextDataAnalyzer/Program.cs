6d9ec4d3b3b7f339027a740e0389df4009a4fa68
dina iprahem <dinaiprahem7@gmail.com>
2024-09-02 06:48:57 +0300

    Create Structure And Relations Between Classes With OOP Principles🤩

---
+++ TextDataAnalyzer/TextDataAnalyzer/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace TextDataAnalyzer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to File Analyzer !!");
+            Console.WriteLine("Please enter your directory path");
+            string path = Console.ReadLine();
+            DirectoryInfo directoryInfo = new DirectoryInfo(path);//convert to directory
+            if (!directoryInfo.Exists)
+            {
+                Console.WriteLine("Folder Not Exist");
+                return;
+            }
+            var files= Directory.GetFiles(path);//return array of files name in the directory
+        }
+    }
+}
